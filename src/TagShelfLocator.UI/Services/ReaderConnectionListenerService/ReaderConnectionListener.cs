acfe33b94d4a4dbf0ad21b3ffada9706f4c5b640
Ryan Blackmore <3052414+rblackmore@users.noreply.github.com>
2024-03-13 16:58:47 +1100

    Fixed an issue where closing the program while inventory loop was running, would cause the FEIG USB driver to become unresponsive until unplugging and replugging in the Reader.

--- src/TagShelfLocator.UI/Services/ReaderConnectionListenerService/ReaderConnectionListener.cs
+++ src/TagShelfLocator.UI/Services/ReaderConnectionListenerService/ReaderConnectionListener.cs
@@ -1,5 +1,6 @@
 ﻿namespace TagShelfLocator.UI.Services.ReaderConnectionListenerService;
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,21 +13,44 @@ using Microsoft.Extensions.Logging;
 
 using TagShelfLocator.UI.Services.ReaderConnectionListenerService.Messages;
 
+using static FEDM.ReaderInfo;
+
 public class ReaderConnectionListener : IHostedService, IUsbListener
 {
   private readonly ILogger<ReaderConnectionListener> logger;
   private readonly IMessenger messenger;
+  private readonly IHostApplicationLifetime lifetime;
 
   private readonly ReaderModule reader;
 
   public ReaderConnectionListener(
     ILogger<ReaderConnectionListener> logger,
     IMessenger messenger,
+    IHostApplicationLifetime lifetime,
     ReaderModule reader)
   {
     this.logger = logger;
     this.reader = reader;
     this.messenger = messenger;
+    this.lifetime = lifetime;
+
+    this.lifetime.ApplicationStopping.Register(HandleGracefulShutdown);
+  }
+
+  private async void HandleGracefulShutdown()
+  {
+    if (!this.reader.isConnected())
+      return;
+
+    var deviceId = this.reader.info().deviceId();
+
+    await SendDisconnectingNotification(deviceId);
+
+    this.reader.disconnect();
+
+    this.SendDisconnectedNotification(deviceId);
+
+    this.reader.Dispose();
   }
 
   public async void onUsbEvent()
@@ -81,19 +105,28 @@ public class ReaderConnectionListener : IHostedService, IUsbListener
 
   private async Task HandleDisconnectedReader(UsbScanInfo scanInfo)
   {
-    // I need to send this before calling 'disconnect()' so I can wait any tasks using the reader.
-    var message = messenger.Send(new ReaderDisconnecting(scanInfo.deviceId()));
+    await SendDisconnectingNotification(scanInfo.deviceId());
+
+    reader.disconnect();
+
+    logger.LogInformation("Reader Disconnected: {deviceId}", scanInfo.deviceId());
+
+    this.SendDisconnectedNotification(scanInfo.deviceId());
+  }
+
+  private async Task SendDisconnectingNotification(uint deviceId)
+  {
+    var message = messenger.Send(new ReaderDisconnecting(deviceId));
 
     // I'm not sure this will totally work,
     // I may receive the message back before all handlers have dealt with it.
     if (message.RunningTasks is not null)
       await Task.WhenAll(message.RunningTasks);
+  }
 
-    reader.disconnect();
-
-    this.messenger.Send(new ReaderDisconnected(scanInfo.deviceId()));
-
-    logger.LogInformation("Reader Disconnected: {deviceId}", scanInfo.deviceId());
+  private void SendDisconnectedNotification(uint deviceId)
+  {
+    this.messenger.Send(new ReaderDisconnected(deviceId));
   }
 
   private bool isReaderDisconnected(UsbScanInfo scanInfo)
