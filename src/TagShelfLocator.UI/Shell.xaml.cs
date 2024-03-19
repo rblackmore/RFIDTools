49c15662a5bae1f9e42537ee992f576f3d0b523a
Ryan Blackmore <3052414+rblackmore@users.noreply.github.com>
2024-03-19 16:12:00 +1100

    Shell Window Title Bar Customized. Window Buttons lack functionality still.

--- src/TagShelfLocator.UI/Shell.xaml.cs
+++ src/TagShelfLocator.UI/Shell.xaml.cs
@@ -1,14 +1,23 @@
 ﻿namespace TagShelfLocator.UI;
 
 using System.Windows;
+using System.Windows.Input;
 
 /// <summary>
 /// Interaction logic for Shell.xaml
 /// </summary>
 public partial class Shell : Window
+{
+  public Shell()
   {
-      public Shell()
-      {
-          InitializeComponent();
-      }
+    InitializeComponent();
   }
+
+  private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+  {
+    if (e.LeftButton == MouseButtonState.Pressed)
+    {
+      DragMove();
+    }
+  }
+}
