44482c872dc05910db03e01fe4ea545bdf600de2
Ryan Blackmore <3052414+rblackmore@users.noreply.github.com>
2024-03-20 15:43:13 +1100

    Shell Window Layout Defined

--- src/TagShelfLocator.UI/Shell.xaml.cs
+++ src/TagShelfLocator.UI/Shell.xaml.cs
@@ -3,6 +3,9 @@
 using System.Windows;
 using System.Windows.Input;
 
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 /// <summary>
 /// Interaction logic for Shell.xaml
 /// </summary>
@@ -20,4 +23,27 @@ public partial class Shell : Window
       DragMove();
     }
   }
+
+  private void WindowClose_Click(object sender, RoutedEventArgs e)
+  {
+    var appLifetime = App.Current.Services.GetRequiredService<IHostApplicationLifetime>();
+
+    appLifetime.StopApplication();
+
+    App.Current.Shutdown();
+  }
+
+  private void WindowMaximize_Click(object sender, RoutedEventArgs e)
+  {
+    if (this.WindowState is not WindowState.Maximized)
+      this.WindowState = WindowState.Maximized;
+    else
+      this.WindowState = WindowState.Normal;
+  }
+
+  private void WindowMinimize_Click(object sender, RoutedEventArgs e)
+  {
+    this.WindowState = WindowState.Minimized;
+  }
+
 }
